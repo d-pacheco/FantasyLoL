dadc22b500238c17674e9949e358395381c4736f
Daylen Pacheco <pacheco.daylen@gmail.com>
2023-12-22 16:07:25 -0700

    mocked out service for match endpoint tests

--- tests/test_endpoints/test_match_endpoint_v1.py
+++ tests/test_endpoints/test_match_endpoint_v1.py
@@ -1,120 +1,121 @@
 from fastapi.testclient import TestClient
 from http import HTTPStatus
+from unittest.mock import patch
 
 from tests.fantasy_lol_test_base import FantasyLolTestBase
 from tests.riot_api_requester_util import RiotApiRequestUtil
-from fantasylol.db.database import DatabaseConnection
+from fantasylol.exceptions.match_not_found_exception import MatchNotFoundException
+from fantasylol.schemas.riot_data_schemas import MatchSchema
 from fantasylol import app
+
+MATCH_ENDPOINT_BASE_URL = "/riot/v1/matches"
+BASE_MATCH_SERVICE_MOCK_PATH = "fantasylol.service.riot_match_service.RiotMatchService"
+GET_MATCHES_MOCK_PATH = f"{BASE_MATCH_SERVICE_MOCK_PATH}.get_matches"
+GET_MATCHES_BY_ID_MOCK_PATH = f"{BASE_MATCH_SERVICE_MOCK_PATH}.get_match_by_id"
 
 
 class MatchEndpointV1Test(FantasyLolTestBase):
     def setUp(self):
         self.client = TestClient(app)
         self.riot_api_util = RiotApiRequestUtil()
-        self.match_in_db = self.create_match_in_db()
 
-    def create_match_in_db(self):
-        mock_match = self.riot_api_util.create_mock_match()
-        with DatabaseConnection() as db:
-            db.add(mock_match)
-            db.commit()
-            db.refresh(mock_match)
-        return mock_match
+    @patch(GET_MATCHES_MOCK_PATH)
+    def test_get_riot_matches_endpoint_search_all(self, mock_get_matches):
+        # Arrange
+        match_db_fixture = self.riot_api_util.create_mock_match()
+        expected_match_response_schema = MatchSchema(**match_db_fixture.to_dict())
+        mock_get_matches.return_value = [match_db_fixture]
 
-    def test_get_riot_matches(self):
-        expected_match_json = self.match_in_db.to_dict()
-        response = self.client.get("riot/v1/matches")
+        # Act
+        response = self.client.get(MATCH_ENDPOINT_BASE_URL)
+
+        # Assert
         self.assertEqual(HTTPStatus.OK, response.status_code)
-
         response_matches = response.json()
         self.assertIsInstance(response_matches, list)
         self.assertEqual(1, len(response_matches))
-        self.assertEqual(expected_match_json, response_matches[0])
+        match_response_schema = MatchSchema(**response_matches[0])
+        self.assertEqual(expected_match_response_schema, match_response_schema)
+        mock_get_matches.assert_called_once_with({
+            "league_name": None,
+            "tournament_id": None
+        })
 
-    def test_get_riot_matches_filter_by_league_name_returns_existing_matches(self):
-        expected_match_json = self.match_in_db.to_dict()
-        league_name = self.match_in_db.league_name
-        response = self.client.get(f"riot/v1/matches?league_name={league_name}")
+    @patch(GET_MATCHES_MOCK_PATH)
+    def test_get_riot_matches_endpoint_filter_by_league_name(self, mock_get_matches):
+        # Arrange
+        match_db_fixture = self.riot_api_util.create_mock_match()
+        expected_match_response_schema = MatchSchema(**match_db_fixture.to_dict())
+        mock_get_matches.return_value = [match_db_fixture]
+
+        # Act
+        response = self.client.get(
+            f"{MATCH_ENDPOINT_BASE_URL}?league_name={match_db_fixture.league_name}"
+        )
+
+        # Assert
         self.assertEqual(HTTPStatus.OK, response.status_code)
-
         response_matches = response.json()
         self.assertIsInstance(response_matches, list)
         self.assertEqual(1, len(response_matches))
-        self.assertEqual(expected_match_json, response_matches[0])
+        match_response_schema = MatchSchema(**response_matches[0])
+        self.assertEqual(expected_match_response_schema, match_response_schema)
+        mock_get_matches.assert_called_once_with({
+            "league_name": match_db_fixture.league_name,
+            "tournament_id": None
+        })
 
-    def test_get_riot_matches_filter_by_league_name_returns_empty_list_when_no_matches_match(self):
-        response = self.client.get("riot/v1/matches?league_name=badName")
+    @patch(GET_MATCHES_MOCK_PATH)
+    def test_get_riot_matches_endpoint_filter_by_tournament_id(self, mock_get_matches):
+        # Arrange
+        match_db_fixture = self.riot_api_util.create_mock_match()
+        expected_match_response_schema = MatchSchema(**match_db_fixture.to_dict())
+        mock_get_matches.return_value = [match_db_fixture]
+
+        # Act
+        response = self.client.get(
+            f"{MATCH_ENDPOINT_BASE_URL}?league_name={match_db_fixture.league_name}"
+        )
+
+        # Assert
         self.assertEqual(HTTPStatus.OK, response.status_code)
-
-        response_matches = response.json()
-        self.assertIsInstance(response_matches, list)
-        self.assertEqual(0, len(response_matches))
-
-    def test_get_riot_matches_filter_by_tournament_id_returns_existing_matches(self):
-        expected_match_json = self.match_in_db.to_dict()
-        tournament_id = self.match_in_db.tournament_id
-        response = self.client.get(f"riot/v1/matches?tournament_id={tournament_id}")
-        self.assertEqual(HTTPStatus.OK, response.status_code)
-
-        response_matches = response.json()
-        self.assertIsInstance(response_matches, list)
-        self.assertEqual(1, len(response_matches))
-        self.assertEqual(expected_match_json, response_matches[0])
-
-    def test_get_riot_matches_filter_tournament_id_returns_empty_list_when_no_matches_match(self):
-        response = self.client.get("riot/v1/matches?tournament_id=777")
-        self.assertEqual(HTTPStatus.OK, response.status_code)
-
-        response_matches = response.json()
-        self.assertIsInstance(response_matches, list)
-        self.assertEqual(0, len(response_matches))
-
-    def test_get_matches_correct_league_name_and_bad_tournament_id_filters_return_empty_list(self):
-        league_name = self.match_in_db.league_name
-        tournament_id = 777
-        request_url = f"riot/v1/matches?league_name={league_name}&tournament_id={tournament_id}"
-        response = self.client.get(request_url)
-        self.assertEqual(HTTPStatus.OK, response.status_code)
-
-        response_matches = response.json()
-        self.assertIsInstance(response_matches, list)
-        self.assertEqual(0, len(response_matches))
-
-    def test_get_matches_bad_league_name_and_correct_tournament_id_filters_return_empty_list(self):
-        league_name = "badName"
-        tournament_id = self.match_in_db.tournament_id
-        request_url = f"riot/v1/matches?league_name={league_name}&tournament_id={tournament_id}"
-        response = self.client.get(request_url)
-        self.assertEqual(HTTPStatus.OK, response.status_code)
-
-        response_matches = response.json()
-        self.assertIsInstance(response_matches, list)
-        self.assertEqual(0, len(response_matches))
-
-    def test_get_matches_correct_league_name_and_tournament_id_filters_return_expected_match(self):
-        league_name = self.match_in_db.league_name
-        tournament_id = self.match_in_db.tournament_id
-        request_url = f"riot/v1/matches?league_name={league_name}&tournament_id={tournament_id}"
-        response = self.client.get(request_url)
-        self.assertEqual(HTTPStatus.OK, response.status_code)
-
-        expected_match_json = self.match_in_db.to_dict()
         response_matches = response.json()
         self.assertIsInstance(response_matches, list)
         self.assertEqual(1, len(response_matches))
-        self.assertEqual(expected_match_json, response_matches[0])
+        match_response_schema = MatchSchema(**response_matches[0])
+        self.assertEqual(expected_match_response_schema, match_response_schema)
+        mock_get_matches.assert_called_once_with({
+            "league_name": None,
+            "tournament_id": match_db_fixture.tournament_id
+        })
 
-    def test_get_matches_by_id_for_existing_match_returns_expected_match(self):
-        expected_match_json = self.match_in_db.to_dict()
-        match_id = self.match_in_db.id
-        response = self.client.get(f"riot/v1/matches/{match_id}")
+    @patch(GET_MATCHES_BY_ID_MOCK_PATH)
+    def test_get_matches_by_id_endpoint_for_existing_match(self, mock_get_match_by_id):
+        # Arrange
+        match_db_fixture = self.riot_api_util.create_mock_match()
+        expected_match_response_schema = MatchSchema(**match_db_fixture.to_dict())
+        mock_get_match_by_id.return_value = match_db_fixture
+
+        # Act
+        response = self.client.get(f"{MATCH_ENDPOINT_BASE_URL}/{match_db_fixture.id}")
+
+        # Assert
         self.assertEqual(HTTPStatus.OK, response.status_code)
-
         response_match = response.json()
-        self.assertEqual(expected_match_json, response_match)
+        self.assertIsInstance(response_match, dict)
+        match_response_schema = MatchSchema(**response_match)
+        self.assertEqual(expected_match_response_schema, match_response_schema)
+        mock_get_match_by_id.assert_called_once_with(match_db_fixture.id)
 
-    def test_get_matches_by_id_for_no_existing_match_returns_not_found_response(self):
-        response = self.client.get("riot/v1/matches/777")
+    @patch(GET_MATCHES_BY_ID_MOCK_PATH)
+    def test_get_matches_by_id_endpoint_for_no_existing_match(self, mock_get_match_by_id):
+        # Arrange
+        match_db_fixture = self.riot_api_util.create_mock_match()
+        mock_get_match_by_id.side_effect = MatchNotFoundException
+
+        # Act
+        response = self.client.get(f"{MATCH_ENDPOINT_BASE_URL}/{match_db_fixture.id}")
+
+        # Assert
         self.assertEqual(HTTPStatus.NOT_FOUND, response.status_code)
-        response_msg = response.json()
-        self.assertEqual("Match not found", response_msg['detail'])
+        mock_get_match_by_id.assert_called_once_with(match_db_fixture.id)
