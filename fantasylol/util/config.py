cd5e38beacf82e7ff50a54102f5c350e11e06b7c
Daylen Pacheco <pacheco.daylen@gmail.com>
2024-01-05 12:51:24 -0700

    Refactored app config (#77)
    * Refactored app config
    
    * Fixed linting issues

--- fantasylol/util/config.py
+++ fantasylol/util/config.py
@@ -2,30 +2,46 @@ import os
 import json
 import sys
 from dotenv import load_dotenv
+from typing import get_type_hints
+from fantasylol.exceptions.AppConfigException import AppConfigException
+
+load_dotenv()
 
 
-class Config:
-    load_dotenv()
-
-    DATABASE_URL: str = os.getenv(
-        'DATABASE_URL',
-        "sqlite:///./fantasy-league-of-legends.db"
-    )
-
+class AppConfig:
+    # Configure config default values
+    DATABASE_URL: str = "sqlite:///./fantasy-league-of-legends.db"
+    DEBUG_LOGGING: bool = False
+    LEAGUE_SERVICE_SCHEDULE: dict = '{"trigger": "cron", "hour": "10", "minute": "00"}'
+    TOURNAMENT_SERVICE_SCHEDULE: dict = '{"trigger": "cron", "hour": "10", "minute": "05"}'
     USE_TEST_DB = 'tests' in sys.argv
 
-    DEBUG_LOGGING: bool = os.getenv('DEBUG_LOGGING', False)
+    def __init__(self, env):
+        for field in self.__annotations__:
+            if not field.isupper():
+                continue
 
-    LEAGUE_SERVICE_SCHEDULE = json.loads(
-        os.getenv(
-            'LEAGUE_SERVICE_SCHEDULE',
-            '{"trigger": "cron", "hour": "10", "minute": "00"}'
-        )
-    )
+            default_value = getattr(self, field, None)
+            if default_value is None and env.get(field) is None:
+                raise AppConfigException(f"The {field} field is required")
 
-    TOURNAMENT_SERVICE_SCHEDULE = json.loads(
-        os.getenv(
-            'TOURNAMENT_SERVICE_SCHEDULE',
-            '{"trigger": "cron", "hour": "10", "minute": "05"}'
-        )
-    )
+            try:
+                var_type = get_type_hints(AppConfig)[field]
+                if var_type == dict:
+                    value = json.loads(env.get(field, default_value))
+                else:
+                    value = var_type(env.get(field, default_value))
+
+                self.__setattr__(field, value)
+            except ValueError:
+                raise AppConfigException(
+                    'Unable to cast value of "{}" to type "{}" for "{}" field'.format(
+                        env[field], var_type, field
+                    )
+                )
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+Config = AppConfig(os.environ)
