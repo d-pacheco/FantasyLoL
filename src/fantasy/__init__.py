4dc57c117e0cf298966a7a49cc1f2dfe0ab1f1e3
Daylen Pacheco <pacheco.daylen@gmail.com>
2024-03-27 15:40:35 -0600

    Riot and fantasy app run in separate containers
    resolves #222

---
+++ src/fantasy/__init__.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi_pagination import add_pagination
+from fastapi_pagination.utils import disable_installed_extensions_check
+
+from .endpoints import router as api_router
+
+
+app = FastAPI()
+add_pagination(app)
+disable_installed_extensions_check()
+app.include_router(api_router)
