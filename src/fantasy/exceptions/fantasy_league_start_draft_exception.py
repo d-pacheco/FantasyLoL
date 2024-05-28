4463833cbcd7ebcac360c6f421e95d3c39ed5cb4
Daylen Pacheco <pacheco.daylen@gmail.com>
2024-05-28 09:06:06 -0700

    Start fantasy draft (#291)
    Owners of their fantasy league can start the Fantasy Draft by moving the fantasy league into `DRAFT` status.
    If the fantasy league has too few or too many members an error is raised. If there are no set available leagues an error is raised. If the fantasy league is not in `PRE_DRAFT` status an error is raised.
    
    resolves #271

---
+++ src/fantasy/exceptions/fantasy_league_start_draft_exception.py
@@ -0,0 +1,10 @@
+from fastapi import HTTPException
+from http import HTTPStatus
+
+
+class FantasyLeagueStartDraftException(HTTPException):
+    def __init__(self, detail):
+        super().__init__(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail=detail
+        )
