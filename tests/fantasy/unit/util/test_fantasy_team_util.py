3d4b78e47f1a8a688593eb49fceb0622f2a5cfaa
Daylen Pacheco <pacheco.daylen@gmail.com>
2024-05-28 17:40:42 -0600

    When all teams are fully drafted move fantasy league to active status
    When all fantasy leagues are fully drafted, the fantasy league is automatically moved to `ACTIVE` status
    
    resolves #270

--- tests/fantasy/unit/util/test_fantasy_team_util.py
+++ tests/fantasy/unit/util/test_fantasy_team_util.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 from tests.test_base import FantasyLolTestBase
 from tests.test_util import fantasy_fixtures
 
-from src.db.models import FantasyLeagueDraftOrderModel
+from src.db.models import FantasyLeagueDraftOrderModel, FantasyTeamModel
 
 from src.fantasy.util.fantasty_team_util import FantasyTeamUtil
 from src.fantasy.exceptions.fantasy_draft_exception import FantasyDraftException
@@ -149,3 +149,217 @@ class TestFantasyTeamUtil(FantasyLolTestBase):
             fantasy_team_util.is_users_position_to_draft(fantasy_league, "notFoundUserId")
         self.assertIn("Could not find draft position", str(context.exception))
         mock_get_fantasy_league_draft_order.assert_called_once_with(fantasy_league.id)
+
+    @patch(f'{BASE_CRUD_PATH}.get_all_fantasy_teams_for_current_week')
+    def test_all_teams_fully_drafted_true(self, mock_get_all_fantasy_teams_for_current_week):
+        # Arrange
+        fantasy_league = fantasy_fixtures.fantasy_league_draft_fixture
+        fantasy_teams = [
+            create_fantasy_team_model(fantasy_league.id, str(user_id), fantasy_league.current_week)
+            for user_id in range(fantasy_league.number_of_teams)
+        ]
+        mock_get_all_fantasy_teams_for_current_week.return_value = fantasy_teams
+
+        # Act
+        all_teams_fully_drafted = fantasy_team_util.all_teams_fully_drafted(fantasy_league)
+
+        # Assert
+        self.assertTrue(all_teams_fully_drafted)
+        self.assertEqual(fantasy_league.number_of_teams, len(fantasy_teams))
+        mock_get_all_fantasy_teams_for_current_week.assert_called_once_with(
+            fantasy_league.id, fantasy_league.current_week
+        )
+
+    @patch(f'{BASE_CRUD_PATH}.get_all_fantasy_teams_for_current_week')
+    def test_all_teams_fully_drafted_too_few_fantasy_teams(
+            self, mock_get_all_fantasy_teams_for_current_week):
+        # Arrange
+        fantasy_league = fantasy_fixtures.fantasy_league_draft_fixture
+        fantasy_teams = [
+            create_fantasy_team_model(fantasy_league.id, str(user_id), fantasy_league.current_week)
+            for user_id in range(fantasy_league.number_of_teams - 1)
+        ]
+        mock_get_all_fantasy_teams_for_current_week.return_value = fantasy_teams
+
+        # Act
+        all_teams_fully_drafted = fantasy_team_util.all_teams_fully_drafted(fantasy_league)
+
+        # Assert
+        self.assertFalse(all_teams_fully_drafted)
+        self.assertNotEqual(fantasy_league.number_of_teams, len(fantasy_teams))
+        mock_get_all_fantasy_teams_for_current_week.assert_called_once_with(
+            fantasy_league.id, fantasy_league.current_week
+        )
+
+    @patch(f'{BASE_CRUD_PATH}.get_all_fantasy_teams_for_current_week')
+    def test_all_teams_fully_drafted_too_many_fantasy_teams(
+            self, mock_get_all_fantasy_teams_for_current_week):
+        # !!!!!!!!!  THIS SHOULD NEVER HAPPEN  !!!!!!!!!!
+        # Arrange
+        fantasy_league = fantasy_fixtures.fantasy_league_draft_fixture
+        fantasy_teams = [
+            create_fantasy_team_model(fantasy_league.id, str(user_id), fantasy_league.current_week)
+            for user_id in range(fantasy_league.number_of_teams + 1)
+        ]
+        mock_get_all_fantasy_teams_for_current_week.return_value = fantasy_teams
+
+        # Act
+        all_teams_fully_drafted = fantasy_team_util.all_teams_fully_drafted(fantasy_league)
+
+        # Assert
+        self.assertFalse(all_teams_fully_drafted)
+        self.assertNotEqual(fantasy_league.number_of_teams, len(fantasy_teams))
+        mock_get_all_fantasy_teams_for_current_week.assert_called_once_with(
+            fantasy_league.id, fantasy_league.current_week
+        )
+
+    @patch(f'{BASE_CRUD_PATH}.get_all_fantasy_teams_for_current_week')
+    def test_all_teams_fully_drafted_team_missing_top_player(
+            self, mock_get_all_fantasy_teams_for_current_week):
+        # Arrange
+        fantasy_league = fantasy_fixtures.fantasy_league_draft_fixture
+        fantasy_teams = [
+            create_fantasy_team_model(fantasy_league.id, str(user_id), fantasy_league.current_week)
+            for user_id in range(fantasy_league.number_of_teams - 1)
+        ]
+        missing_top_player_team = create_fantasy_team_model(
+            fantasy_league.id, "123", fantasy_league.current_week, set_top_player_id=False
+        )
+        fantasy_teams.append(missing_top_player_team)
+        mock_get_all_fantasy_teams_for_current_week.return_value = fantasy_teams
+
+        # Act
+        all_teams_fully_drafted = fantasy_team_util.all_teams_fully_drafted(fantasy_league)
+
+        # Assert
+        self.assertFalse(all_teams_fully_drafted)
+        self.assertEqual(fantasy_league.number_of_teams, len(fantasy_teams))
+        self.assertEqual(missing_top_player_team.top_player_id, None)
+        mock_get_all_fantasy_teams_for_current_week.assert_called_once_with(
+            fantasy_league.id, fantasy_league.current_week
+        )
+
+    @patch(f'{BASE_CRUD_PATH}.get_all_fantasy_teams_for_current_week')
+    def test_all_teams_fully_drafted_team_missing_jungle_player(
+            self, mock_get_all_fantasy_teams_for_current_week):
+        # Arrange
+        fantasy_league = fantasy_fixtures.fantasy_league_draft_fixture
+        fantasy_teams = [
+            create_fantasy_team_model(fantasy_league.id, str(user_id), fantasy_league.current_week)
+            for user_id in range(fantasy_league.number_of_teams - 1)
+        ]
+        missing_jungle_player_team = create_fantasy_team_model(
+            fantasy_league.id, "123", fantasy_league.current_week, set_jungle_player_id=False
+        )
+        fantasy_teams.append(missing_jungle_player_team)
+        mock_get_all_fantasy_teams_for_current_week.return_value = fantasy_teams
+
+        # Act
+        all_teams_fully_drafted = fantasy_team_util.all_teams_fully_drafted(fantasy_league)
+
+        # Assert
+        self.assertFalse(all_teams_fully_drafted)
+        self.assertEqual(fantasy_league.number_of_teams, len(fantasy_teams))
+        self.assertEqual(missing_jungle_player_team.jungle_player_id, None)
+        mock_get_all_fantasy_teams_for_current_week.assert_called_once_with(
+            fantasy_league.id, fantasy_league.current_week
+        )
+
+    @patch(f'{BASE_CRUD_PATH}.get_all_fantasy_teams_for_current_week')
+    def test_all_teams_fully_drafted_team_missing_mid_player(
+            self, mock_get_all_fantasy_teams_for_current_week):
+        # Arrange
+        fantasy_league = fantasy_fixtures.fantasy_league_draft_fixture
+        fantasy_teams = [
+            create_fantasy_team_model(fantasy_league.id, str(user_id), fantasy_league.current_week)
+            for user_id in range(fantasy_league.number_of_teams - 1)
+        ]
+        missing_mid_player_team = create_fantasy_team_model(
+            fantasy_league.id, "123", fantasy_league.current_week, set_mid_player_id=False
+        )
+        fantasy_teams.append(missing_mid_player_team)
+        mock_get_all_fantasy_teams_for_current_week.return_value = fantasy_teams
+
+        # Act
+        all_teams_fully_drafted = fantasy_team_util.all_teams_fully_drafted(fantasy_league)
+
+        # Assert
+        self.assertFalse(all_teams_fully_drafted)
+        self.assertEqual(fantasy_league.number_of_teams, len(fantasy_teams))
+        self.assertEqual(missing_mid_player_team.mid_player_id, None)
+        mock_get_all_fantasy_teams_for_current_week.assert_called_once_with(
+            fantasy_league.id, fantasy_league.current_week
+        )
+
+    @patch(f'{BASE_CRUD_PATH}.get_all_fantasy_teams_for_current_week')
+    def test_all_teams_fully_drafted_team_missing_adc_player(
+            self, mock_get_all_fantasy_teams_for_current_week):
+        # Arrange
+        fantasy_league = fantasy_fixtures.fantasy_league_draft_fixture
+        fantasy_teams = [
+            create_fantasy_team_model(fantasy_league.id, str(user_id), fantasy_league.current_week)
+            for user_id in range(fantasy_league.number_of_teams - 1)
+        ]
+        missing_adc_player_team = create_fantasy_team_model(
+            fantasy_league.id, "123", fantasy_league.current_week, set_adc_player_id=False
+        )
+        fantasy_teams.append(missing_adc_player_team)
+        mock_get_all_fantasy_teams_for_current_week.return_value = fantasy_teams
+
+        # Act
+        all_teams_fully_drafted = fantasy_team_util.all_teams_fully_drafted(fantasy_league)
+
+        # Assert
+        self.assertFalse(all_teams_fully_drafted)
+        self.assertEqual(fantasy_league.number_of_teams, len(fantasy_teams))
+        self.assertEqual(missing_adc_player_team.adc_player_id, None)
+        mock_get_all_fantasy_teams_for_current_week.assert_called_once_with(
+            fantasy_league.id, fantasy_league.current_week
+        )
+
+    @patch(f'{BASE_CRUD_PATH}.get_all_fantasy_teams_for_current_week')
+    def test_all_teams_fully_drafted_team_missing_support_player(
+            self, mock_get_all_fantasy_teams_for_current_week):
+        # Arrange
+        fantasy_league = fantasy_fixtures.fantasy_league_draft_fixture
+        fantasy_teams = [
+            create_fantasy_team_model(fantasy_league.id, str(user_id), fantasy_league.current_week)
+            for user_id in range(fantasy_league.number_of_teams - 1)
+        ]
+        missing_support_player_team = create_fantasy_team_model(
+            fantasy_league.id, "123", fantasy_league.current_week, set_support_player_id=False
+        )
+        fantasy_teams.append(missing_support_player_team)
+        mock_get_all_fantasy_teams_for_current_week.return_value = fantasy_teams
+
+        # Act
+        all_teams_fully_drafted = fantasy_team_util.all_teams_fully_drafted(fantasy_league)
+
+        # Assert
+        self.assertFalse(all_teams_fully_drafted)
+        self.assertEqual(fantasy_league.number_of_teams, len(fantasy_teams))
+        self.assertEqual(missing_support_player_team.support_player_id, None)
+        mock_get_all_fantasy_teams_for_current_week.assert_called_once_with(
+            fantasy_league.id, fantasy_league.current_week
+        )
+
+
+def create_fantasy_team_model(
+        fantasy_league_id: str,
+        user_id: str,
+        week: int,
+        set_top_player_id: bool = True,
+        set_jungle_player_id: bool = True,
+        set_mid_player_id: bool = True,
+        set_adc_player_id: bool = True,
+        set_support_player_id: bool = True) -> FantasyTeamModel:
+    return FantasyTeamModel(
+        fantasy_league_id=fantasy_league_id,
+        user_id=user_id,
+        week=week,
+        top_player_id=f"topId{user_id}" if set_top_player_id else None,
+        jungle_player_id=f"jungleId{user_id}" if set_jungle_player_id else None,
+        mid_player_id=f"midId{user_id}" if set_mid_player_id else None,
+        adc_player_id=f"adcId{user_id}" if set_adc_player_id else None,
+        support_player_id=f"supportId{user_id}" if set_support_player_id else None,
+    )
