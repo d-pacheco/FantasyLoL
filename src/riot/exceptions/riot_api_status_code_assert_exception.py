1eea61986325737a8b31ef095a757b42a45b7609
Daylen Pacheco <pacheco.daylen@gmail.com>
2024-06-03 11:06:09 -0600

    Updated riot util to use new custom IDs

--- src/riot/exceptions/riot_api_status_code_assert_exception.py
+++ src/riot/exceptions/riot_api_status_code_assert_exception.py
@@ -1,11 +1,15 @@
 from ...common.exceptions.fantasy_lol_exception import FantasyLolException
+from typing import List
 
 
 class RiotApiStatusCodeAssertException(FantasyLolException):
-    def __init__(self, expected: int, received: int, url: str) -> None:
-        expected = expected
-        received = received
-        url = url
-        error_msg = f"Received unexpected status code. " \
-                    f"Wanted {expected} but got {received} in {url}"
+    def __init__(self, expected: List[int], received: int, url: str) -> None:
+        base_error_msg = "Received unexpected status code. "
+        if len(expected) == 1:
+            status_code_error_msg = f"Wanted {expected[0]} but got {received} in {url}"
+        if len(expected) > 1:
+            status_code_error_msg = f"Wanted one of {expected} but got {received} in {url}"
+        else:
+            status_code_error_msg = "No status code provided in logic..."
+        error_msg = base_error_msg + status_code_error_msg
         super().__init__(error_msg)
