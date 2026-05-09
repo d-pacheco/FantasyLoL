dd7b6a1bff2b74492c620819302496889df345da
Daylen Pacheco <pacheco.daylen@gmail.com>
2026-05-08 22:29:02 -0600

    chore: improve docker_server.sh to run from repo root (#429)
    Accepts up/down/restart commands so you don't need to cd into docker-files or remember the full compose command.

--- scripts/docker_server.sh
+++ scripts/docker_server.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+set -e
 
-cd docker-files || exit 1
-docker compose -p fantasy_lol -f docker-compose.server.yml --env-file ../.env up -d --build
+COMPOSE="docker compose -p fantasy_lol -f docker-files/docker-compose.server.yml --env-file .env"
+
+case "${1:-up}" in
+  up)
+    $COMPOSE up -d --build
+    ;;
+  down)
+    $COMPOSE down
+    ;;
+  restart)
+    $COMPOSE down
+    $COMPOSE up -d --build
+    ;;
+  *)
+    echo "Usage: $0 {up|down|restart}"
+    exit 1
+    ;;
+esac
